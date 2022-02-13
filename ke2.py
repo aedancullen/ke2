ca88431980e1688e720c63a29ed648c666cc38dd
Aedan Cullen <aedancullen@gmail.com>
2022-02-13 15:59:31 -0600

    Update

--- ke2.py
+++ ke2.py
@@ -43,26 +43,25 @@ def postprocess(song_blob, beat_steps, chunksize_min, chunksize_max):
     tempo, beats = librosa.beat.beat_track(onset_envelope = song_blob["orig_onset_strength"])
     print(tempo)
     
+    step_idx = np.interp(np.arange(0, beats.size, 1/beat_steps), np.arange(0, beats.size, 1), beats)
+    step_idx = np.unique(np.rint(step_idx).astype(np.int32))
+    
     vocals_chroma = librosa.feature.chroma_stft(S=np.abs(song_blob["vocals_stft"])**2)
     other_chroma = librosa.feature.chroma_stft(S=np.abs(song_blob["other_stft"])**2)
     bass_chroma = librosa.feature.chroma_stft(S=np.abs(song_blob["bass_stft"])**2)
-    drums_melspectrogram = librosa.feature.melspectrogram(S=np.abs(song_blob["drums_stft"])**2)
-    
-    step_idx = np.interp(np.arange(0, beats.size, 1/beat_steps), np.arange(0, beats.size, 1), beats)
-    step_idx = np.rint(step_idx).astype(np.int32)
     
     cat_feat = np.concatenate((vocals_chroma, other_chroma, bass_chroma), axis=0)
-    step_feat = np.split(cat_feat, np.unique(step_idx), axis=1)
-
+    
+    step_feat = np.split(cat_feat, step_idx, axis=1)
     step_feat_mean = np.stack(list(map(lambda c: np.mean(c, axis=1), step_feat)), axis=-1)
     
     best_stat = -np.inf
     best_chunksize = None
     best_norm = None
-    for i in range(chunksize_min, chunksize_max):
+    for i in range(chunksize_min, chunksize_max + 1):
         diff = step_feat_mean - np.roll(step_feat_mean, i * beat_steps, axis=1)
         norm = np.linalg.norm(diff, ord=2, axis=0)
-        stat = np.var(norm)
+        stat = -np.sum(norm, axis=0)
         if stat > best_stat:
             best_stat = stat
             best_chunksize = i
@@ -70,13 +69,13 @@ def postprocess(song_blob, beat_steps, chunksize_min, chunksize_max):
     
     print(best_chunksize, best_stat)
     
-    maxbeat = step_feat_mean.shape[1] // beat_steps
+    maxbeat = best_norm.size // beat_steps
     
     best_stat = -np.inf
     best_offset = None
-    for i in range(best_chunksize, maxbeat - best_chunksize):
-        sum_l = np.mean(best_norm[(i - best_chunksize) * beat_steps : (i) * beat_steps], axis=0)
-        sum_r = np.mean(best_norm[(i) * beat_steps : (i + best_chunksize) * beat_steps], axis=0)
+    for i in range(best_chunksize, maxbeat - best_chunksize + 1):
+        sum_l = np.sum(best_norm[(i - best_chunksize) * beat_steps : (i) * beat_steps], axis=0)
+        sum_r = np.sum(best_norm[(i) * beat_steps : (i + best_chunksize) * beat_steps], axis=0)
         stat = sum_l - sum_r
         if stat > best_stat:
             best_stat = stat
