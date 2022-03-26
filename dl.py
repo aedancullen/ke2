9125c59ea739d0af950dd3133c9ed79146613b12
Aedan Cullen <aedan@aedancullen.com>
2022-03-26 13:12:16 -0500

    Add dl.py (only clean file atm)

---
+++ dl.py
@@ -0,0 +1,34 @@
+import string
+import csv
+import requests
+import subprocess
+
+url_prefix = "/watch?v="
+session = requests.Session()
+
+def process(trackid):
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"}
+    url = "https://www.youtube.com/results"
+    response = session.get(url, params={"search_query": trackid + " audio"}, headers=headers).text
+    idx = response.find(url_prefix)
+    video_id = response[idx + len(url_prefix) : idx + len(url_prefix) + 11]
+    topurl = "https://www.youtube.com" + url_prefix + video_id
+    print("====>", trackid, topurl)
+    subprocess.run('yt-dlp -f bestaudio[asr=44100] --extract-audio --audio-format mp3 --output "dl/' + trackid + '.%(ext)s" ' + topurl, shell=True, stdout=subprocess.DEVNULL)
+
+trackids = []
+
+with open("hot100.csv", 'r') as file:
+    csv = csv.reader(file, quotechar='"', delimiter=',', quoting=csv.QUOTE_ALL, skipinitialspace=True)
+    next(csv) # clear first line
+    for line in csv:
+        url,weekid,week_position,song,performer,songid,instance,previous_week_position,peak_position,weeks_on_chart = tuple(line)
+        year = int(weekid[-4:])
+        song = ''.join([c for c in song if c in string.ascii_letters or c in string.whitespace or c in string.digits])
+        performer = ''.join([c for c in performer if c in string.ascii_letters or c in string.whitespace or c in string.digits])
+        trackid = song + ' ' + performer
+        if not year >= 2000:
+            continue
+        if not trackid in trackids:
+            trackids.append(trackid)
+            process(trackid)
