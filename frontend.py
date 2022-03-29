aa1ab64d956efe6ad302c7ef321501464e361ba7
Aedan Cullen <aedan@aedancullen.com>
2022-03-29 03:30:37 -0500

    Move old stuff to ke2_bk

--- frontend.py
+++ frontend.py
@@ -4,8 +4,8 @@ from spleeter.audio.adapter import AudioAdapter
 
 audio_loader = AudioAdapter.default()
 
-sr = 44100
-audio, _ = audio_loader.load(sys.argv[1], sample_rate=sr)
+audio, sr = audio_loader.load(sys.argv[1])
 
-song_blob = ke2.preprocess(audio, sr)
-chunks, chunksize = ke2.postprocess(song_blob, 4, 4, 32)
+song_blob = ke2.preprocess(audio, int(sr))
+best_chunksize, best_offset = ke2.postprocess(song_blob, 4, 8, 32)
+print(best_chunksize, best_offset)
